134893bb94dbc71c4baa24fc5d3fb94c5b917a09
Isaac D2 <hi@isaacd2.com>
2026-02-08 09:14:08 -0500

    fixed a flow issue

--- main.py
+++ main.py
@@ -197,9 +197,11 @@ def drawCard(p, flip = False):
     if userInput.lower() == 'b':
         score(p)
         endRoundTurn(p)
+        return
     if userInput.lower() == 'lb':
         print_leaderboard(False)
         drawCard(p, flip)
+        return
     card = userInput.lower()
     numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
     additions = ["+2", "+4", "+6", "+8", "+10"]
